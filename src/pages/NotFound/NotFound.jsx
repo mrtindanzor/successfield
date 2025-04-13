1029c412644e528613638d82ea2b250ed15494c8
Mr. Tindanzor <63526717+mrtindanzor@users.noreply.github.com>
2025-04-13 12:11:59 +0000

    404 page redesigned

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,15 +1,14 @@
-import styles from "./NotFound.module.css";
 import { Link } from "react-router-dom";
 
 export default function NotFound(){
 
   return (
-    <div className={styles.notFound}>
-      <h2> 404 </h2>
+    <div className=" grid gap-5 w-[95%] max-w-[500px] mx-auto relative top-20 text-center">
+      <h2 className=" texturina text-6xl text-red-500 "> 404 </h2>
       <p>
         The page or resource you are looking for has either been moved or does not exist.
       </p>
-      <Link to='/' > Back to homepage </Link>
+      <Link to='/' className=" py-2 px-3 rounded bg-green-200 hover:bg-green-500 hover:text-white transition duration-150 ease-out w-fit justify-self-center  " > Back to homepage </Link>
     </div>
   )
 }
