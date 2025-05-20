e503e1bd335c5a9dd5792543257980abc7d3fefe
Mr. Tindanzor <63526717+mrtindanzor@users.noreply.github.com>
2025-05-20 01:15:13 +0000

    added logo to admin page

--- src/Admin/Components/Header.jsx
+++ src/Admin/Components/Header.jsx
@@ -1,13 +1,15 @@
 import { Menu } from 'lucide-react'
 import useAuth from './../../Contexts/AuthenticationContext'
+import LogoElement from '../../Components/Header/Logo'
 
 export default function Header(){
   const { userFullName } = useAuth()
 
   return (
     <div 
-      className="bg-white sm:col-span-2 flex gap-5 items-center font-bold sticky top-0 left-0 text-2xl text-gray-800 texturina w-full z-999 overflow-hidden rounded-b whitespace-nowrap py-1 sm:py-3 px-2"
+      className="bg-white sm:col-span-2 flex gap-1 items-center font-bold sticky top-0 left-0 text-2xl text-gray-800 texturina w-full z-999 overflow-hidden rounded-b whitespace-nowrap px-2"
       >
+      <LogoElement />
       <div
         className="overflow-hidden"
         >
