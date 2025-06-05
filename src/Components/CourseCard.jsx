6979c20856a8421d72279be55ee9e00be624b532
Mr. Tindanzor <63526717+mrtindanzor@users.noreply.github.com>
2025-06-05 01:43:46 +0000

    updated course card

--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -4,13 +4,13 @@ import { capitalize, createAcronym } from "../core";
 export default function CourseCard({ pic = '', title, overview, isCourse }){
   const courseLink = title.toLowerCase().trim().split(' ').join('_')
   const courseAcronym = createAcronym(title)
-  const placeholder = pic ? <img src={ pic } alt={ courseAcronym } className=" " /> : <div className=" flex justify-center text-gray-800 w-full h-full flex-1 py-5 border-b-1 border-b-green-700 object-contain text-2xl texturina "> { courseAcronym } </div>
 
   return (
-    <div className={`bg-green-500/50 rounded-md grid gap-y-5 pt-10 pb-20 px-5 [box-shadow:2px_2px_5px_gray] min-h-80 overflow-hidden ${ isCourse && 'w-80 mx-auto' }`} >
-      { placeholder }
-      <h3 className="text-2xl text-white [text-shadow:1px_1px_1px_black] font-bold capitalize"> { title } </h3>
-      { overview && <Link to={ '/courses/' + courseLink } className="bg-white w-fit whitespace-nowrap h-fit py-2 px-5 mt-10 rounded-sm font-semibold text-xl hover:bg-transparent hover:border-1"> Start course </Link> }
+    <div className={`bg-gray-800 rounded-md grid grid-rows-[auto_1fr_auto] gap-y-5 pt-5 pb-10 px-5 [box-shadow:2px_2px_5px_gray] min-h-80 overflow-hidden ${ isCourse && 'w-80 mx-auto' }`} >
+      { pic && <img src={ pic } alt={ courseAcronym } className=" " /> }
+      { !pic && <div className="flex text-white w-full h-full flex-1 py-5 border-b-1 border-b-green-700 object-contain text-2xl texturina "> { courseAcronym } </div> }
+      <h3 className="text-white capitalize"> { title } </h3>
+      { overview && <Link to={ '/courses/' + courseLink } className="text-gray-400 border-2 w-fit whitespace-nowrap h-fit py-2 px-5 mt-5 rounded-sm font-semibold text-xl hover:bg-gray-600 hover:text-white"> Start course </Link> }
     </div>
   )
 }
