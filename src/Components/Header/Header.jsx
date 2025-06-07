f628ee9346c193fbb26e6e541d4ea8dab728accb
Mr. Tindanzor <63526717+mrtindanzor@users.noreply.github.com>
2025-06-07 23:43:19 +0000

    added find a professional

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,12 +9,12 @@ export default function Header(){
   const [ navbarActive, setNavbarActive ] = useState(false)
 
   return (
-    <header className="sticky px-5 sm:px-8 md:px-10 md:py-2 bg-white z-2 w-[100vw] gap-x-2 top-0 left-0 md:h-22 grid grid-cols-[auto_1fr_auto_auto] md:grid-cols-[1fr_auto_auto] md:grid-rows-[auto_1fr] justify-start items-center md:items-start">
+    <header className="sticky px-5 sm:px-8 md:px-10 md:py-2 bg-white z-2 w-[100vw] gap-x-2 top-0 left-0 md:h-22 grid grid-cols-[auto_1fr_auto] md:grid-cols-[1fr_auto_auto] md:grid-rows-[auto_1fr] justify-start items-center md:items-start">
       <MenuButton { ...{ navbarActive, setNavbarActive } } />
       <LogoElement />
       <Link
         to="/find-professional"
-        className='block  px-3 rounded font-bold border-2 text-black border-gray-200 hover:border-gray-400 py-2 w-fit h-fit bg-gray-100 hover:bg-gray-300'
+        className='hidden md:block px-3 rounded font-bold border-2 text-black border-gray-200 hover:border-gray-400 py-2 w-fit h-fit bg-gray-100 hover:bg-gray-300'
       > Find a professional </Link>
       <AuthButtons />
       <Navbar { ...{ coursesActive, setCoursesActive, navbarActive, setNavbarActive } } />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -36,7 +36,7 @@ export default function Navbar({ coursesActive, setCoursesActive, navbarActive, 
     { title: 'Verify Certificate' },
     { title: 'Accreditations' },
     { title: 'Faqs' },
-    { title: 'About us' },
+    // { title: 'About us' },
     // { title: 'Contact us' }
   ]
 
@@ -54,92 +54,96 @@ export default function Navbar({ coursesActive, setCoursesActive, navbarActive, 
         <ul 
           className="relative flex flex-col md:flex-row bg-white md:bg-transparent h-full z-2"
           >
-          { 
-            !isLoggedIn && <li>
+          { !isLoggedIn && <li>
               <Link to="/users/join" 
-              className="md:hidden flex bg-red-500 py-2 px-5 sm:px-8 md:px-10 uppercase items-center gap-2 text-white font-bold hover:bg-red-800 " 
-              onClick={ () => {
-                            coursesActive && setCoursesActive(false)
-                            navbarActive && setNavbarActive(false)
-                          } }>
+                className="md:hidden flex bg-red-500 py-2 px-5 sm:px-8 md:px-10 uppercase items-center gap-2 text-white font-bold hover:bg-red-800 " 
+                onClick={ () => {
+                  coursesActive && setCoursesActive(false)
+                  navbarActive && setNavbarActive(false)
+                } }>
                 <FileText />
                 Apply
               </Link>
-            </li> 
-          }
-          {
-          MenuItems.map( menu => {
-            let path = "/" + menu.title.toLowerCase().split(' ').join('-')
-            if(menu.title.toLocaleLowerCase() === 'home') path=''
-            return (
-              <li 
-                key={ menu.title } 
-                className="md:relative"
-                >
+            </li> }
+          <Link
+            to="/find-professional"
+            onClick={ () => {
+              coursesActive && setCoursesActive(false)
+              navbarActive && setNavbarActive(false)
+            } }
+            className='md:hidden rounded font-bold text-black px-5 sm:px-8 py-2 bg-gray-100 hover:bg-gray-200'
+          > Find a professional </Link>
+          { MenuItems.map( menu => {
+              let path = "/" + menu.title.toLowerCase().split(' ').join('-')
+              if(menu.title.toLocaleLowerCase() === 'home') path=''
+              return (
+                <li 
+                  key={ menu.title } 
+                  className="md:relative"
+                  >
 
-                { !menu.list && <NavLink 
-                  to={ path } 
-                  className="block text-gray-900 py-2 md:py-0 px-5 sm:px-8 md:px-3 font-semibold hover:bg-gray-200 w-full whitespace-nowrap" 
-                  onClick={ () => navbarActive && setNavbarActive(false) }
-                  > { menu.title } </NavLink> }
+                  { !menu.list && <NavLink 
+                    to={ path } 
+                    className="block text-gray-900 py-2 md:py-0 px-5 sm:px-8 md:px-3 font-semibold hover:bg-gray-200 w-full whitespace-nowrap" 
+                    onClick={ () => navbarActive && setNavbarActive(false) }
+                    > { menu.title } </NavLink> }
 
-                { menu.list && <span 
-                      className=" text-gray-900 font-semibold flex items-center justify-between cursor-pointer py-2 md:py-0 px-5 sm:px-8 md:px-3 w-full hover:bg-gray-200 capitalize"
-                      onClick={ () => setCoursesActive(prev => !prev) }
-                      > { menu.title }
-                      <ChevronRight 
-                        className="md:rotate-[90deg]"/>
-                    </span> 
-                }
+                  { menu.list && <span 
+                        className=" text-gray-900 font-semibold flex items-center justify-between cursor-pointer py-2 md:py-0 px-5 sm:px-8 md:px-3 w-full hover:bg-gray-200 capitalize"
+                        onClick={ () => setCoursesActive(prev => !prev) }
+                        > { menu.title }
+                        <ChevronRight 
+                          className="md:rotate-[90deg]"/>
+                      </span> 
+                  }
 
-                {
-                  menu.list && <ul
-                      className={`sub-nav bg-white md:top-[100%] md:max-h-[60vh] md:border-t-2
-                        ${ coursesActive ? 'absolute top-0 z-1 h-full w-full md:h-fit overflow-y-scroll md:overflow-[unset] md:w-fit' : 'hidden' }
-                         `}
-                      onClick={ () => setCoursesActive(false) }
-                      >
-                      <span 
-                        className="sticky cursor-pointer top-0 z-1 bg-white flex items-center font-4xl font-bold capitalize py-2 px-5 border-b-1 border-gray-500 md:hidden">
-                        <ChevronLeft />
-                        go back
-                      </span>
-                      {
-                        menu.list.map((list, listIndex) => {
-                          path = "/" + menu.title.toLowerCase().split(' ').join('_') + "/" + list.course.toLowerCase().split(' ').join('_')
+                  {
+                    menu.list && <ul
+                        className={`sub-nav bg-white md:top-[100%] md:max-h-[60vh] md:border-t-2
+                          ${ coursesActive ? 'absolute top-0 z-1 h-full w-full md:h-fit overflow-y-scroll md:overflow-[unset] md:w-fit' : 'hidden' }
+                          `}
+                        onClick={ () => setCoursesActive(false) }
+                        >
+                        <span 
+                          className="sticky cursor-pointer top-0 z-1 bg-white flex items-center font-4xl font-bold capitalize py-2 px-5 border-b-1 border-gray-500 md:hidden">
+                          <ChevronLeft />
+                          go back
+                        </span>
+                        {
+                          menu.list.map((list, listIndex) => {
+                            path = "/" + menu.title.toLowerCase().split(' ').join('_') + "/" + list.course.toLowerCase().split(' ').join('_')
 
-                          return (
-                            <li
-                              key={ listIndex }
-                              >
-                              { listIndex === 0 && <Link 
-                                  to="/courses"
-                                    className="md:pl-5 md:pr-10 py-2 px-5 w-[500px] font-semibold bg-red-500 text-white hidden md:block hover:bg-gray-950 hover:text-white" 
-                                    onClick={ () => {
-                                      coursesActive && setCoursesActive(false)
-                                      coursesActive && setNavbarActive(false)
-                                    } }
-                                  > View all courses
-                                </Link> }
-                              <NavLink
-                                to={path}
-                                className={ `block md:pl-5 md:pr-10 py-2 px-5 w-full  md:w-[500px] hover:bg-gray-950 hover:!text-white capitalize after:!border-none !font-normal` }
-                                onClick={ () => {
-                                  coursesActive && setCoursesActive(false)
-                                  navbarActive && setNavbarActive(false)
-                                } }
-                                > { list.course }
-                              </NavLink>
-                            </li>
-                          )
-                        })
-                      }
-                    </ul>
-                }
-              </li>
-            )
-          })
-        }
+                            return (
+                              <li
+                                key={ listIndex }
+                                >
+                                { listIndex === 0 && <Link 
+                                    to="/courses"
+                                      className="md:pl-5 md:pr-10 py-2 px-5 w-[500px] font-semibold bg-red-500 text-white hidden md:block hover:bg-gray-950 hover:text-white" 
+                                      onClick={ () => {
+                                        coursesActive && setCoursesActive(false)
+                                        coursesActive && setNavbarActive(false)
+                                      } }
+                                    > View all courses
+                                  </Link> }
+                                <NavLink
+                                  to={path}
+                                  className={ `block md:pl-5 md:pr-10 py-2 px-5 w-full  md:w-[500px] hover:bg-gray-950 hover:!text-white capitalize after:!border-none !font-normal` }
+                                  onClick={ () => {
+                                    coursesActive && setCoursesActive(false)
+                                    navbarActive && setNavbarActive(false)
+                                  } }
+                                  > { list.course }
+                                </NavLink>
+                              </li>
+                            )
+                          })
+                        }
+                      </ul>
+                  }
+                </li>
+              )
+            }) }
         </ul>
         <div className={`${ coursesActive ? '' : navbarActive ? '' : 'hidden' } h-[100vh] z-1 w-[300vw] 
             md:!fixed md:top-[100%] md:left-0 md:right-0 md:translate-x-[-50%] md:bg-gray-950/20 bg-gray-950/60`
